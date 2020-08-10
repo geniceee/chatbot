b9db474503a9bb9504cacb052101bb9f1ce35836
geniceee <59923023+geniceee@users.noreply.github.com>
2020-08-10 17:54:27 +0800

    stylistic changes - routed sidepanel, cleaned up front end code, added pictures and did the filter search function

--- chat/models.py
+++ chat/models.py
@@ -1,18 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+from datetime import datetime
 from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
 User = get_user_model()
 
-# class Document(models.Model):
-#     # docfile = models.FileField(upload_to='documents/')
-#     name = models.CharField(max_length=50) 
-#     docimage = models.ImageField(upload_to='images/')
-
 class Contact(models.Model):
     user = models.ForeignKey(User, related_name='friends', on_delete=models.CASCADE)
     friends = models.ManyToManyField('self', blank=True)
+    msg_file = models.FileField(blank=True, null=True, upload_to='profilepic', storage=FileSystemStorage())
 
     def __str__(self):
         return self.user.username
@@ -20,27 +18,46 @@ class Contact(models.Model):
 class Message(models.Model):
     contact = models.ForeignKey(Contact, related_name='messages', on_delete=models.CASCADE)
     content = models.TextField()
-    # timestamp = models.DateField(blank=False, default=datetime.now().strftime("%Y-%m-%d, %H:%M:%S"))
     timestamp = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.contact.user.username
 
+    def render_timestamp(self):
+        prefix = ""
+        timeDiff = round(datetime.now.time() - self.timestamp.time() / 60000)
+
+        if timeDiff < 1:
+            prefix = "just now"
+        elif timeDiff < 60 and timeDiff > 1:
+            prefix = "{} minutes ago".format(timeDiff)
+        elif timeDiff < 24*60 and timeDiff > 60:
+            prefix = "{} hours ago".format(round(timeDiff / 60))
+        elif timeDiff < 31 * 24 * 60 and timeDiff > 24 * 60:
+            prefix = "{} days ago".format(round(timeDiff / (60 * 24)))
+        else:
+            prefix = str(self.timestamp)
+
+        return prefix
+
 class Chat(models.Model):
     participants = models.ManyToManyField(Contact, related_name='chats')
     messages = models.ManyToManyField(Message, blank=True)
-    msg_file = models.FileField(blank=True,null=True,
-        upload_to='profilepic',storage=FileSystemStorage())
 
     def __str__(self):
         return "{}".format(self.pk) 
-
+    
     def most_recent_message(self):
         return self.messages.order_by('-timestamp').all()[0].content
+
+    def most_recent_contact(self):
+        return self.messages.order_by('-timestamp').all()[0].contact
+
+    def other_contacts(self):
+        return self.participants.all()
 
     def other_participants(self):
         return list(map(str,self.participants.all()))
 
-    # def other_participants(self, contact):
-    #     return list(map(str,self.participants.exclude(contact)))
+    def get_absolute_url(self):
+        return reverse('room', args=[str(self.pk)])
--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,13 @@
-from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
 from django.shortcuts import render, get_object_or_404
 from django.utils.safestring import mark_safe
 import json
-from .models import Message, Chat, Contact
-# from .forms import DocumentForm
+from .models import Chat, Contact, Message
 from django.contrib.auth import get_user_model
-from datetime import datetime
+from django.http import HttpResponseRedirect
+from chatbot import settings
+import os
+from django.http import JsonResponse
 
 User = get_user_model()
 
@@ -20,38 +20,19 @@ def index(request):
     author = request.user.username
     contact = get_user_contact(author)
     queryset = contact.chats.all()
-    chats = queryset.values()
 
     context = {
-        'username': mark_safe(json.dumps(request.user.username)),
-        'filtered_chats': chats,
+        'filtered_chats': queryset,
+        'author': author,
+        'contact': contact,
     }
+
     return render(request, 'chat/index.html', context=context)
 
 def get_user_contact(username):
     user = get_object_or_404(User, username=username)
     return get_object_or_404(Contact, user=user)
 
-# def check_profile(func):
-#     def decorator(func):
-#     # @wraps(func)
-#         def wrapper(arg_1, arg_2):
-#             context = {
-#             'room_name_json': mark_safe(json.dumps(arg_2)),
-#             }
-#             requested_chat = Chat.objects.get(id=arg_2)
-#             query_participants = requested_chat.participants.all()
-#             count = 0
-#             for participant in query_participants:
-#                 user = request.user.username
-#                 participant_name = participant.user.username
-#                 if user == participant_name:
-#                     count += 1
-#             if count==0:
-#                 logout(request)
-#                 return HttpResponseRedirect('/accounts/logout/')
-#             return wrapper
-#         return decorator
 
 def check_profile(func):
     def wrapper(request, room_name):
@@ -67,10 +48,12 @@ def check_profile(func):
             if user == participant_name:
                 count += 1
         if count==0:
+            # logout(request)
             return HttpResponseRedirect('/accounts/logout/')
         
         return func(request, room_name)
     return wrapper
+
 
 @login_required
 @check_profile
@@ -81,75 +64,45 @@ def room(request, room_name):
     contact = get_user_contact(author)
     queryset = contact.chats.all()
 
-    msg_list= []
+    requested_chat = Chat.objects.get(id=room_name)
+    query_participants = requested_chat.participants.all()
 
-    for chat in queryset:
-        msg_list = chat.messages.order_by('-timestamp').all()
-        last_msg_author = msg_list[0]
-        last_msg = last_msg_author.content
-    
     context = {
         'room_name_json': mark_safe(json.dumps(room_name)),
         'username': mark_safe(json.dumps(request.user.username)),
         'filtered_chats': queryset,
         'author': author,
-        'messages': msg_list
+        'contact': contact,
+        'participants': query_participants
     }
 
     return render(request, 'chat/room.html', context=context)
 
-# def room(request, room_name):
-#     queryset = Chat.objects.all()
-#     author = request.user.username
-#     contact = get_user_contact(author)
-#     queryset = contact.chats.all()
-#     chats = queryset.values()
-
-#     context = {
-#         'room_name_json': mark_safe(json.dumps(room_name)),
-#         'username': mark_safe(json.dumps(request.user.username)),
-#         'filtered_chats': chats,
-#     }
-
-#     return render(request, 'chat/room.html', context=context)
-
-
 def get_last_10_messages(room_name):
     chat = get_object_or_404(Chat, id=room_name)
-    return chat.messages.order_by('-timestamp').all()
+    return chat.messages.order_by('timestamp').all()
 
 def get_current_chat(room_name):
     return get_object_or_404(Chat, id=room_name)
 
-# def image_view(request):
-#     # Handle file upload
-#     print('success')
-#     if request.method == 'POST':
-#         form = DocumentForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             newdoc = Document(docfile = request.FILES['docfile'])
-#             newdoc.save()
-#             return redirect('success') 
-#     else: 
-#         form = DocumentForm() 
-#     return render(request, 'room.html', {'form' : form}) 
-  
-  
-# def success(request): 
-#     return HttpResponse('successfully uploaded') 
-
 def profile_photo(request):
     data=request.FILES.get('file')
 
-    author = request.user.username
-    if author.profile_photo:
-        authpath = author.profile_photo.url.split(settings.MEDIA_URL)
-        os.remove(os.path.join(settings.MEDIA_ROOT,authpath[1]))
-    #data.name = str(request.user.id) + "_apfp"
-    author.profile_photo=data
+    user = request.user
+    author = Contact.objects.get(user=user)
+
+    if author.msg_file:
+        advpath = author.msg_file.url.split(settings.MEDIA_URL)
+        os.remove(os.path.join(settings.MEDIA_ROOT,advpath[1]))
+    
+    # data.name = str(request.user.id) + "_apfp"
+    author.msg_file=data
     author.save()
 
     data = {
         'ok' : 'ok',
     }
     return JsonResponse(data)
+
+
+
