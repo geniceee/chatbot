907aa991bc152a6f8e7e0e52962532285e5c2982
Ng Huan Ran <huanran@dimuto.io>
2021-02-02 09:15:16 +0800

    fix migration and added gitignore

--- chat/migrations/0003_auto_20200716_0723.py
+++ chat/migrations/0003_auto_20200716_0723.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('chat', '0002_auto_20200716_0719'),
+        ('chat', '0002_auto_20200801_0809'),
     ]
 
     operations = [
